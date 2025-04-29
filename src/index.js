a7d3f2d96bf6f6a7aa521fc1bf0a32197ef28d5e
chowon442 <chowon442@gmail.com>
2025-04-30 05:49:56 +0900

    feat: create shop, region files (Controller, Service, Repository, DTO)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { signupController } from "./controllers/user.controller.js";
+import { regionController } from "./controllers/region.controller.js";
+import { shopController } from "./controllers/shop.controller.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,8 @@ const port = process.env.PORT || 3000;
 const router = express.Router();
 const authRouter = express.Router();
 const signupRouter = express.Router();
+const regionsRouter = express.Router();
+const shopsRouter = express.Router();
 
 app.use(cors());
 app.use(express.static("public")); // 정적 파일을 제공하는 미들웨어 설정
@@ -28,8 +32,17 @@ signupRouter.patch("/terms", signupController.updateTerms);
 signupRouter.patch("/foods", signupController.updateFoods);
 signupRouter.patch("/status", signupController.updateStatus);
 
+// "/regions/{region_id}/shops" 경로에 대한 라우터 설정
+regionsRouter.post("/:region_id/shops", regionController.createShopByRegionId);
+
+// "/shops/{shop_id}/missions" 경로에 대한 라우터 설정
+shopsRouter.post("/:shop_id/missions", shopController.createMissionByShopId);
+shopsRouter.post("/:shop_id/reviews", shopController.createReviewByShopId);
+
 authRouter.use("/signup", signupRouter);
 router.use("/auth", authRouter);
+router.use("/regions", regionsRouter);
+router.use("/shops", shopsRouter);
 app.use("/api/v1", router);
 
 app.listen(port, () => {
